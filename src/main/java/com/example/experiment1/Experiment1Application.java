dc035f5f9334f668c2d24e1ccf0b7b4e720ac856
Sondre Garnæs <sondre433@gmail.com>
2025-08-27 10:17:22 +0200

    Commit

--- src/main/java/com/example/experiment1/Experiment1Application.java
+++ src/main/java/com/example/experiment1/Experiment1Application.java
@@ -13,7 +13,7 @@ public class Experiment1Application {
     public static void main(String[] args) {
         SpringApplication.run(Experiment1Application.class, args);
     }
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
