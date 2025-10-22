375fa2a9de1b888d4507073d1735b4b0359cc2d9
ranuru <126164337+ranuru@users.noreply.github.com>
2025-10-22 18:34:15 +0200

    Refactor Neo4jPollService: add Docker run command comments and ensure driver is closed after use

--- code/src/main/java/com/example/experiment1/service/Neo4jPollService.java
+++ code/src/main/java/com/example/experiment1/service/Neo4jPollService.java
@@ -3,6 +3,16 @@ package com.example.experiment1.service;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.GraphDatabase;
 
+// terminal command to run the docker container on linux (if other OS remove "sudo")
+// sudo docker run \
+//   --name neo4j-db \
+//   --publish=7474:7474 --publish=7687:7687 \
+//   --volume=$HOME/neo4j/data:/data \
+//   --env NEO4J_AUTH=neo4j/supersecret \
+//   neo4j:latest
+
+
+
 public class Neo4jPollService {
     public static void main(String... args) {
         final String dbUri = ("bolt://localhost:7687");
@@ -12,6 +22,7 @@ public class Neo4jPollService {
         try (var driver = GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword))) {
             driver.verifyConnectivity();
             System.out.println("Connection established");
+            driver.close();
         }
     }
 
