8157be59026735f21b7f4883edfbaf5ddd0d4f58
ranuru <126164337+ranuru@users.noreply.github.com>
2025-10-28 18:18:51 +0100

    Refactor Experiment1Application and PollCacheRepository to improve structure and ensure proper annotations

--- code/src/main/java/com/example/experiment1/Experiment1Application.java
+++ code/src/main/java/com/example/experiment1/Experiment1Application.java
@@ -2,17 +2,15 @@ package com.example.experiment1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
 @RestController
+@EnableNeo4jRepositories("com.example.experiment1.repository")
 public class Experiment1Application {
     public static void main(String[] args) {
         SpringApplication.run(Experiment1Application.class, args);
     }
-    
+
 }
-
-
--- code/src/main/java/com/example/experiment1/repository/PollCacheRepository.java
+++ code/src/main/java/com/example/experiment1/repository/PollCacheRepository.java
@@ -1,9 +1,11 @@
 package com.example.experiment1.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.experiment1.domain.PollAggregate;
 
+@Repository
 public interface PollCacheRepository extends Neo4jRepository<PollAggregate, String> {
 
 }
