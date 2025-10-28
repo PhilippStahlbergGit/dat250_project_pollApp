f6dffac40d4828d640d005669c92be92f85730ee
ranuru <126164337+ranuru@users.noreply.github.com>
2025-10-28 19:48:30 +0100

    Refactor PollCache interface to include documentation of methods

--- code/src/main/java/com/example/experiment1/cache/PollCache.java
+++ code/src/main/java/com/example/experiment1/cache/PollCache.java
@@ -1,10 +1,44 @@
 package com.example.experiment1.cache;
 
+import java.util.Map;
+
+import com.example.experiment1.domain.PollAggregate;
+
 public interface PollCache {
 
-    void recordVote(String pollId, String option);
+    /**
+     * Record a vote for a given poll and option.
+     * 
+     * @param pollId the poll ID
+     * @param option the option being voted on
+     * @param count  the number of votes (can be negative to decrement)
+     */
+    void updateVote(String pollId, String option, int count);
 
-    java.util.Map<String, Integer> getAggregatedResults(String pollId);
+    /**
+     * Get aggregated results for a given poll.
+     * 
+     * @param pollId the poll ID
+     * @return a map of option captions to their vote counts
+     */
+    Map<String, Integer> getAggregatedResults(String pollId);
 
+    /**
+     * Clean up stale poll aggregates that haven't been updated recently.
+     */
     void cleanStaleAggregates();
+
+    /**
+     * Save a new poll aggregate to the cache.
+     * 
+     * @param pollAggregate the poll aggregate to save
+     */
+    void savePoll(PollAggregate pollAggregate);
+
+    /**
+     * Remove a poll aggregate from the cache.
+     * 
+     * @param pollId the poll ID to remove
+     */
+    void removePoll(String pollId);
 }
