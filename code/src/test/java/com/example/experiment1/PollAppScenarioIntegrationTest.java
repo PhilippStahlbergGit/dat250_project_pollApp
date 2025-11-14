45b409d153539c43941d6c9a5c9c14bcc6dbe2f7
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2025-11-14 16:37:34 +0100

    Small notes and testdb added

--- code/src/test/java/com/example/experiment1/PollAppScenarioIntegrationTest.java
+++ code/src/test/java/com/example/experiment1/PollAppScenarioIntegrationTest.java
@@ -20,129 +20,131 @@ import org.springframework.web.client.RestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PollAppScenarioIntegrationTest {
 
-    @LocalServerPort
-    private int port;
+	// TODO: Remark, these tests are now out of expiry, meaning they will not work
+	// for the updated software.
+	@LocalServerPort
+	private int port;
 
-    private RestClient client;
-    private String baseUrl;
-    private User user1;
-    private User user2;
-    private Poll poll;
-    private Vote vote;
+	private RestClient client;
+	private String baseUrl;
+	private User user1;
+	private User user2;
+	private Poll poll;
+	private Vote vote;
 
-    @BeforeEach
-    public void Initiate() {
-        client = RestClient.create();
-        baseUrl = "http://localhost:" + port;
+	@BeforeEach
+	public void Initiate() {
+		client = RestClient.create();
+		baseUrl = "http://localhost:" + port;
 
-        // Creating user1
-        user1 = new User();
-        user1.setUsername("sondre");
-        user1.setEmail("sondremail");
-        client.post()
-                .uri(baseUrl + "/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(user1)
-                .retrieve()
-                .toBodilessEntity();
-        ;
-        // Creating user2
-        user2 = new User();
-        user2.setUsername("tia");
-        user2.setEmail("tiamail");
-        client.post()
-                .uri(baseUrl + "/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(user2)
-                .retrieve()
-                .toBodilessEntity();
-        ;
-        // Creating a poll
-        VoteOption option1 = new VoteOption();
-        option1.setCaption("Grønn");
-        option1.setPresentationOrder(1);
+		// Creating user1
+		user1 = new User();
+		user1.setUsername("sondre");
+		user1.setEmail("sondremail");
+		client.post()
+				.uri(baseUrl + "/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(user1)
+				.retrieve()
+				.toBodilessEntity();
+		;
+		// Creating user2
+		user2 = new User();
+		user2.setUsername("tia");
+		user2.setEmail("tiamail");
+		client.post()
+				.uri(baseUrl + "/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(user2)
+				.retrieve()
+				.toBodilessEntity();
+		;
+		// Creating a poll
+		VoteOption option1 = new VoteOption();
+		option1.setCaption("Grønn");
+		option1.setPresentationOrder(1);
 
-        VoteOption option2 = new VoteOption();
-        option2.setCaption("Blå");
-        option2.setPresentationOrder(2);
+		VoteOption option2 = new VoteOption();
+		option2.setCaption("Blå");
+		option2.setPresentationOrder(2);
 
-        poll = new Poll();
-        poll.setQuestion("Favorittfarge?");
-        poll.setOptions(Arrays.asList(option1, option2));
+		poll = new Poll();
+		poll.setQuestion("Favorittfarge?");
+		poll.setOptions(Arrays.asList(option1, option2));
 
-        // user1 creates the poll
-        client.post()
-                .uri(baseUrl + "/polls/" + user1.getUserId())
-                .body(poll)
-                .retrieve()
-                .toBodilessEntity();
+		// user1 creates the poll
+		client.post()
+				.uri(baseUrl + "/polls/" + user1.getUserId())
+				.body(poll)
+				.retrieve()
+				.toBodilessEntity();
 
-        // Voting on option 1
-        vote = new Vote();
-        vote.setOptionIndex(1);
-        client.post()
-                .uri(baseUrl + "/vote/" + user1.getUserId() + "/" + poll.getPollId())
-                .body(vote)
-                .retrieve()
-                .toBodilessEntity();
+		// Voting on option 1
+		vote = new Vote();
+		vote.setOptionIndex(1);
+		client.post()
+				.uri(baseUrl + "/vote/" + user1.getUserId() + "/" + poll.getPollId())
+				.body(vote)
+				.retrieve()
+				.toBodilessEntity();
 
-    }
+	}
 
-    @Test
-    public void checkUsers() {
-        // Checking that the users were created
-        User[] users = client.get()
-                .uri(baseUrl + "/users")
-                .retrieve()
-                .body(User[].class);
-        assertThat(users)
-                .extracting(User::getUsername)
-                .contains(user1.getUsername(), user2.getUsername());
-    }
+	@Test
+	public void checkUsers() {
+		// Checking that the users were created
+		User[] users = client.get()
+				.uri(baseUrl + "/users")
+				.retrieve()
+				.body(User[].class);
+		assertThat(users)
+				.extracting(User::getUsername)
+				.contains(user1.getUsername(), user2.getUsername());
+	}
 
-    @Test
-    public void checkPolls() {
-        // Get polls
-        Poll[] polls = client.get()
-                .uri(baseUrl + "/polls")
-                .retrieve()
-                .body(Poll[].class);
-        assertThat(polls)
-                .extracting(Poll::getQuestion)
-                .contains(poll.getQuestion());
-    }
+	@Test
+	public void checkPolls() {
+		// Get polls
+		Poll[] polls = client.get()
+				.uri(baseUrl + "/polls")
+				.retrieve()
+				.body(Poll[].class);
+		assertThat(polls)
+				.extracting(Poll::getQuestion)
+				.contains(poll.getQuestion());
+	}
 
-    @Test
-    public void changeVoteAndCheck() {
-        // Changing vote to option 2
-        vote.setOptionIndex(2);
-        client.post()
-                .uri(baseUrl + "/vote/" + user1.getUserId() + "/" + poll.getPollId())
-                .body(vote)
-                .retrieve()
-                .toBodilessEntity();
+	@Test
+	public void changeVoteAndCheck() {
+		// Changing vote to option 2
+		vote.setOptionIndex(2);
+		client.post()
+				.uri(baseUrl + "/vote/" + user1.getUserId() + "/" + poll.getPollId())
+				.body(vote)
+				.retrieve()
+				.toBodilessEntity();
 
-        // Check to see the vote is for option 2
-        Vote[] votes = client.get()
-                .uri(baseUrl + "/vote")
-                .retrieve()
-                .body(Vote[].class);
-        assertThat(votes)
-                .extracting(Vote::getOptionIndex)
-                .contains(vote.getOptionIndex());
-    }
+		// Check to see the vote is for option 2
+		Vote[] votes = client.get()
+				.uri(baseUrl + "/vote")
+				.retrieve()
+				.body(Vote[].class);
+		assertThat(votes)
+				.extracting(Vote::getOptionIndex)
+				.contains(vote.getOptionIndex());
+	}
 
-    @Test
-    public void deletePollAndCheckVoteIsDeleted() {
-        // Delete poll
-        client.delete()
-                .uri(baseUrl + "/polls/delete/" + poll.getPollId())
-                .retrieve()
-                .toBodilessEntity();
-        Vote[] deleted_votes = client.get()
-                .uri(baseUrl + "/vote")
-                .retrieve()
-                .body(Vote[].class);
-        assertThat(deleted_votes).isEmpty();
-    }
+	@Test
+	public void deletePollAndCheckVoteIsDeleted() {
+		// Delete poll
+		client.delete()
+				.uri(baseUrl + "/polls/delete/" + poll.getPollId())
+				.retrieve()
+				.toBodilessEntity();
+		Vote[] deleted_votes = client.get()
+				.uri(baseUrl + "/vote")
+				.retrieve()
+				.body(Vote[].class);
+		assertThat(deleted_votes).isEmpty();
+	}
 }
